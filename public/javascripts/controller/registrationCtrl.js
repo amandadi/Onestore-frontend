e07405cc753f1152024b386225373fbe9abc9c51
amandadi <amar.mnd@gmail.com>
2014-09-06 20:55:36 +0530

    Registration Page Functionally Working. Validation Form. Error Handling in node.js

--- public/javascripts/controller/registrationCtrl.js
+++ public/javascripts/controller/registrationCtrl.js
@@ -1,21 +1,24 @@
 'use strict';
 
-app.controller('registrationCtrl',function($scope,$location) {
+app.controller('registrationCtrl',function($scope,$location, $http) {
 
   $scope.types = ['User','Vendor'];
   var userInfo;
+
   $scope.save = function(user){
+
     console.log(user);
+    $http.post('/registrationCheck',user)
+    .then(function(msg){
+
+      if(msg.data=='error'){
+      $location.path('/error');
+      }
+      console.log(msg.data);
+    });
+
   };
 
-  $scope.getCssClasses = function(ngModelContoller) {
-    return {
-      error: ngModelContoller.$invalid && ngModelContoller.$dirty,
-      success: ngModelContoller.$valid && ngModelContoller.$dirty
-    };
-  };
-  
-  $scope.showError = function(ngModelController, error) {
-    return ngModelController.$error[error];
-  };
+ 
 });
+
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var Client = require('node-rest-client').Client;
+
+var client = new Client();
 
 /* GET home page. */
 
@@ -17,6 +20,38 @@ router.get('/login', function(req, res) {
 
 router.get('/registration', function(req, res) {
   res.render('registration', {});
+});
+
+router.post('/registrationCheck',function(req,res){
+
+var body=req.body;
+var address=body.address;
+
+delete body.address;
+var addlist=[];
+addlist.push(address);
+
+body.userAddresses=addlist;
+var data=JSON.stringify(body);
+
+var args = {data:data,headers:{"Content-Type": "application/json"} };
+
+client.post("http://localhost:8080/OneStoreServiceRouter/userinfo", args, function(data,response) {
+      // parsed response body as js object
+    console.log('**************'+data);
+    // raw response
+    console.log('###############'+response);
+res.send('registrationsuccess');
+}
+    ).on('error',function(err){
+            console.log('something went wrong on the request', err.request.options);
+            res.send('error');
+        });
+
+});
+
+router.get('/error', function(req, res) {
+  res.render('error', {});
 });
 
 router.get('/vendor-form', function(req, res) {
